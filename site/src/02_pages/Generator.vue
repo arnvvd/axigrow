6b8b1153c3c5d261096e6c94c9bb03c123b661ca
Julien Espagnon <julien.espagnon@gmail.com>
2017-11-11 16:32:39 +0100

    Add form

--- site/src/02_pages/Generator.vue
+++ site/src/02_pages/Generator.vue
@@ -1,5 +1,9 @@
 <template>
-    <div style="margin-top: 5rem;">
-        <h1>Generator</h1>
-    </div>
+	<form class="generator__form" action="">
+    	<input class="input--name" type="text" name="fullname" placeholder="@username twitter">
+    	<div class="submit__buttons">
+	    	<input class="input--submit input--submit-01" type="submit" value="Generate your shape">
+	    	<input class="input--submit input--submit-02" type="submit" value="Print your shape*">
+    	</div>
+    </form>
 </template>
--- site/src/02_pages/Home.vue
+++ site/src/02_pages/Home.vue
@@ -1,6 +1,27 @@
 <template>
 	<div class="intro__wrapper">
-		
+		<div>
+			<div class="letter__container">
+				<p class="font__logo">G</p>
+			</div>
+			<div class="letter__container">
+				<p class="font__logo">E</p>
+			</div>
+			<div class="letter__container">
+				<p class="font__logo">N</p>
+			</div>
+		</div>
+		<div>
+			<div class="letter__container">
+				<p class="font__logo">I</p>
+			</div>
+			<div class="letter__container">
+				<p class="font__logo">•</p>
+			</div>
+			<div class="letter__container">
+				<p class="font__logo">D</p>
+			</div>
+		</div>
 	</div>
 </template>
 
