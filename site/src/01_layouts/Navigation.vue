0cd1e74b4d48657378f47c122a4e2e9fcaceaacb
Arnvvd <pinot.arnaud@gmail.com>
2017-11-15 03:23:11 +0100

    Prepare header transition axigrow page (black page)

--- site/src/01_layouts/Navigation.vue
+++ site/src/01_layouts/Navigation.vue
@@ -3,7 +3,21 @@
         <ul>
             <li>
                 <router-link to="/">Home</router-link>
-                <router-link to="/about">About</router-link>
+            </li>
+            <li>
+            	<router-link to="/about">About</router-link>
+            </li>
+            <li>
+            	<router-link to="/generator">Generator</router-link>
+            </li>
+            <li>
+            	<router-link to="/shapes">Shapes</router-link>
+            </li>
+            <li>
+            	<router-link to="/dashboard">Dashboard</router-link>
+            </li>
+            <li>
+            	<router-link to="/axigrow">Axigrow</router-link>
             </li>
         </ul>
     </nav>
